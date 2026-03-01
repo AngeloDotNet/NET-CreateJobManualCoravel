ae90fd9f56e2fc76cffedff67dd4c5d3707c8620
Angelo Pirola <angelo9871@gmail.com>
2026-03-01 09:52:10 +0100

    Add Message entity class to Example2.Entities namespace
    Introduced a new Message class with properties for Id, To, Subject, Body, and SentAt. String properties are initialized to non-null defaults to ensure proper usage. This class will be used to represent messages within the application.

---
+++ src/Example2/Entities/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Example2.Entities;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string To { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string Body { get; set; } = null!;
+    public DateTime SentAt { get; set; }
+}
