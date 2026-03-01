177e7c23d7cdcef6806e26d0b5e85def4508254c
Angelo Pirola <angelo9871@gmail.com>
2026-03-01 09:52:02 +0100

    Add AppDbContext with Message entity configuration
    Introduced AppDbContext in the Example2.Data namespace, inheriting from DbContext. Added a DbSet<Message> for entity operations and configured the Message entity's primary key in OnModelCreating.

---
+++ src/Example2/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Example2.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Example2.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public virtual DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Message>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+        });
+    }
+}
